335d14a756fbcb03476f453a683d3569f2b42ab7
even <786007448@qq.com>
2016-12-08 23:27:28 +0800

    2.1

--- 4.3/pipe.c
+++ 4.3/pipe.c
@@ -5,6 +5,19 @@
 #include<sys/wait.h>
 #define MAXVALUE 100
 
+void output(int num,char cache[])
+{
+	int i;
+	printf("%d types ",num);
+	if(num!=0){
+		for(i=0;i<num;i++)
+		{
+			printf("%c",cache[i]);
+		}
+	}
+	printf("\n");
+}
+ 
 int main()
 {
 	int fd[2];//0--read 1--write
@@ -15,28 +28,24 @@ int main()
 	}
 	while((pid1=fork())==-1);//fork success
 	if(pid1==0){//child process
-		printf("In child1 process\n");
+		printf("In child1 process...Write 'Child1'\n");
 		close(fd[0]);
-		lockf(fd[1],F_LOCK,0);//lock write
 		write(fd[1],"Child1 ",7);
-		sleep(3);//5 s
-		lockf(fd[1],F_ULOCK,0);//unlock
+		sleep(3);//3 s
 		exit(0);
 	}
 	else if(pid1>0){
 		while((pid2=fork())==-1);
 		if(pid2==0){
-			printf("In child2 process\n");
+			printf("In child2 process...Write 'Child2'\n");
 			close(fd[0]);
-			//lockf(fd[1],1,0);
 			write(fd[1],"Child2 ",7);
 			sleep(3);
-			//lockf(fd[1],0,0);
 			exit(0);
 		}else if(pid2>0){
 			while((pid3=fork())==-1);
 			if(pid3==0){
-				printf("In child3 process\n");
+				printf("In child3 process...Write 'Child3'\n");
 				close(fd[0]);
 				write(fd[1],"Child3 ",7);
 				sleep(3);
@@ -46,14 +55,17 @@ int main()
 				printf("In parent process\n");
 				close(fd[1]);
 				wait(0);//wait for child end
+				printf("First read 14 type\n");
+				num =read(fd[0],cache,14);
+				output(num,cache);
+				
+				printf("Second read 7 type\n");
 				num =read(fd[0],cache,7);
-				printf("%d type %s\n",num,cache);
-				//wait(0);
+				output(num,cache);
+				
+				printf("Third read 7 type\n");
 				num =read(fd[0],cache,7);
-				printf("%d type %s\n",num,cache);
-				//wait(0);
-				num =read(fd[0],cache,7);
-				printf("%d type %s\n",num,cache);
+				output(num,cache);
 				exit(0);
 			}
 		}
